7d468a52c950683ca2e669b365f273f357cb32a3
silentl007 <xtreemsilentl@gmail.com>
2021-05-05 19:33:54 +0100

    created login logic, added alerts and others

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,10 @@
+import 'package:bankapp/home.dart';
+import 'package:bankapp/model.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 class Login extends StatelessWidget {
+  LoginLogic loginClass = LoginLogic();
   final usernameControl = TextEditingController();
   final passwordControl = TextEditingController();
   final _key = GlobalKey<FormState>();
@@ -8,6 +12,7 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        appBar: AppBar(),
           body: Form(
         key: _key,
         child: Column(children: [
@@ -34,5 +39,78 @@ class Login extends StatelessWidget {
         ]),
       )),
     );
+  }
+
+  login(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) => FutureBuilder(
+              future: loginClass.login(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState != ConnectionState.done) {
+                  return Container(
+                    color: Colors.transparent,
+                    child: AlertDialog(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      content: LinearProgressIndicator(
+                        backgroundColor: Colors.white,
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.purple),
+                      ),
+                    ),
+                  );
+                } else if (snapshot.data == 200) {
+                  // login
+                  loginSuccess(context);
+                  return Container();
+                } else if (snapshot.data == 404) {
+                  // return error
+                  loginFailed(context);
+                  return Container();
+                } else {
+                  loginNetwork(context);
+                  return Container();
+                }
+              },
+            ));
+  }
+
+  loginSuccess(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pop(context);
+      return Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    });
+  }
+
+  loginFailed(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pop(context);
+      return AwesomeDialog(
+        context: context,
+        dialogType: DialogType.ERROR,
+        animType: AnimType.BOTTOMSLIDE,
+        title: 'Error - Unable to Login',
+        desc: 'Incorrect login details',
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {},
+      )..show();
+    });
+  }
+
+  loginNetwork(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Navigator.pop(context);
+      return AwesomeDialog(
+        context: context,
+        dialogType: DialogType.ERROR,
+        animType: AnimType.BOTTOMSLIDE,
+        title: 'Error - Unable to Login',
+        desc: 'Please check your network connection',
+        btnCancelOnPress: () {},
+        btnOkOnPress: () {},
+      )..show();
+    });
   }
 }
--- lib/model.dart
+++ lib/model.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class LoginLogic {
   String username;
@@ -9,10 +10,17 @@ class LoginLogic {
       'phoneNumber': username,
       'password': password
     };
-    String link = '';
+    Uri link = Uri.parse('');
     try {
       var encodeData = jsonEncode(loginDetails);
+      var send = await http.post(link,
+          body: encodeData,
+          headers: {'Content-Type': 'application/json; charset=UTF-8'});
+      print('=======> body: ${send.body}');
+      print('=======> status: ${send.statusCode}');
     } catch (e) {
+      print('=======> error: $e');
+      return null;
     }
   }
 }
