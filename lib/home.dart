214d3d254efcf18002dca6b42ceece8751be2170
silentl007 <xtreemsilentl@gmail.com>
2021-05-07 06:44:58 +0100

    still on home design

--- lib/home.dart
+++ lib/home.dart
@@ -18,23 +18,102 @@ class _HomeState extends State<Home> {
             context, MaterialPageRoute(builder: (context) => Login()));
       },
       child: Scaffold(
+          backgroundColor: UserColors.blackbackground,
           appBar: userWidgets.userappbar(Icons.home),
           body: Column(
-            children: [accountDetails()],
+            children: [
+              accountDetails(),
+              Divider(),
+              Expanded(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        child: Row(
+                          children: [
+                            Expanded(
+                                flex: 1,
+                                child: Container(
+                                  height: double.infinity,
+                                  width: double.infinity,
+                                  decoration: decorContainer(),
+                                  child: Center(
+                                    child: columnContainer(
+                                        'Send Money', Icons.send, 70, 30),
+                                  ),
+                                )),
+                            VerticalDivider(),
+                            Expanded(
+                                flex: 1,
+                                child: Container(
+                                  height: double.infinity,
+                                  width: double.infinity,
+                                  decoration: decorContainer(),
+                                )),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: double.infinity,
+                        width: double.infinity,
+                        decoration: decorContainer(),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+            ],
           )),
     ));
+  }
+
+  columnContainer(
+      String text, IconData icon, double iconSize, double textSize) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          color: UserColors.yellowColor,
+          size: iconSize,
+        ),
+        Text(
+          text,
+          style: TextStyle(
+              color: UserColors.yellowColor,
+              fontSize: textSize,
+              fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  decorContainer() {
+    return BoxDecoration(
+        border: Border.all(
+          color: UserColors.yellowColor,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(30)));
   }
 
   accountDetails() {
     return Card(
       margin: EdgeInsets.all(10.0),
-      elevation: 1.0,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(50.0))),
       child: Container(
-          decoration: BoxDecoration(
-              gradient: RadialGradient(
-                  colors: [Color(0xFF015FFF), Color(0xFF015FFF)])),
+          color: UserColors.blackbackground,
           padding: EdgeInsets.all(5.0),
           // color: Color(0xFF015FFF),
           child: Column(
@@ -45,16 +124,17 @@ class _HomeState extends State<Home> {
                   IconButton(
                     icon: Icon(
                       Icons.arrow_back,
-                      color: Colors.white,
+                      color: UserColors.yellowColor,
                     ),
                     onPressed: () {},
                   ),
                   Text("Savings",
-                      style: TextStyle(color: Colors.white, fontSize: 20.0)),
+                      style: TextStyle(
+                          color: UserColors.yellowColor, fontSize: 20.0)),
                   IconButton(
                     icon: Icon(
                       Icons.arrow_forward,
-                      color: Colors.white,
+                      color: UserColors.yellowColor,
                     ),
                     onPressed: () {},
                   )
@@ -64,7 +144,8 @@ class _HomeState extends State<Home> {
                 child: Padding(
                   padding: EdgeInsets.all(5.0),
                   child: Text(r"$ " "95,940.00",
-                      style: TextStyle(color: Colors.white, fontSize: 24.0)),
+                      style: TextStyle(
+                          color: UserColors.yellowColor, fontSize: 24.0)),
                 ),
               ),
               SizedBox(height: 35.0),
